2dc73ae7b9926562a962b62738b7ce1d6e9ad687
Mark Stickel <mark.stickel@verifi.com>
2020-10-08 15:03:43 -0700

    Project setup

---
+++ src/main/java/com/markstickel/testcontainers/App.java
@@ -0,0 +1,13 @@
+package com.markstickel.testcontainers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
